618725ed3c8a2c3191d5672e33ef0248254235a6
Maryna Stognienko <maryna@mindwell.cz>
2024-12-10 11:48:20 +0100

    homework2

---
+++ node_basics/la.js
@@ -0,0 +1,4 @@
+import chalk from "chalk";
+
+
+console.log(chalk.blue('Lorem ipsum ') + chalk.bgGreen(' dolor sit amet, consectetuer adipiscing elit.') + chalk.green('Etiam posuere lacus quis dolor.'));
