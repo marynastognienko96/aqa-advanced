19644b45c9cd328e2495fb8b0458f7140fc52359
Maryna Stognienko <maryna@mindwell.cz>
2024-12-12 09:38:02 +0100

    littleCorrection

--- jsBasics/task4.js
+++ jsBasics/task4.js
@@ -1,20 +1,23 @@
 // 4.1
-let radiusOfCircle = 23;
-let pi = Math.PI;
-let areaOfACircle = pi * radiusOfCircle^2
+let radiusOfCircle = 23.5;
+const pi = Math.PI;
+let areaOfCircle = pi * radiusOfCircle ^ 2
+let roundedAreaOfCircle = areaOfCircle.toFixed(2);
 
-console.log(areaOfACircle);
+console.log(roundedAreaOfCircle);
 
 // 4.2
-let length = 10;
-let width = 5;
+const length = 10.5;
+let width = 5.2;
 let areaOfRectangle = length * width;
+let roundedAreaOfRectangle = areaOfRectangle.toFixed(2);
 
-console.log(areaOfRectangle);
+console.log(roundedAreaOfRectangle);
 
 // 4.3
 let radiusOfCylinder = 12.3;
-let heightOfCylinder = 25;
-let volumeOfCylinder = pi * radiusOfCylinder^2 * heightOfCylinder;
+let heightOfCylinder = 25.2;
+let volumeOfCylinder = pi * radiusOfCylinder ^ 2 * heightOfCylinder;
+let roundedVolume = volumeOfCylinder.toFixed(2);
 
-console.log(volumeOfCylinder);
+console.log(roundedVolume);
